77d40d7424abfc2d643c0439b9fec9fc0b7ec4f9
JStiiven <johnortega13579@gmail.com>
2024-02-25 20:39:13 -0500

    Implementacion operacion multiplicar

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function sumar(num1, num2) {
 function restar(num1, num2) {
     return num1 - num2
 }
+
+function multiplicar(num1, num2) {
+    return num1 * num2
+}
   
   // Función principal del programa
   function calculadora() {
