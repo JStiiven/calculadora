28c849baa3e3659b70e67fb1afd5b998e094e989
JStiiven <johnortega13579@gmail.com>
2024-02-25 20:43:29 -0500

    Implementacion de operacion division

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ function restar(num1, num2) {
 function multiplicar(num1, num2) {
     return num1 * num2
 }
+
+function dividir(num1, num2) {
+    return num1 / num2
+}
   
   // Función principal del programa
   function calculadora() {
